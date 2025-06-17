a4ef430c06c6168fd62e11ec625e1fb7a7c6391b
Positiveoo1 <github0512@gmail.com>
2025-06-17 09:46:27 +0200

    Initial Commit

---
+++ WorkTracker/WorkTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTrackerApp.swift
+//  WorkTracker
+//
+//  Created by Abubakrsiddik Abdurakhimov on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
